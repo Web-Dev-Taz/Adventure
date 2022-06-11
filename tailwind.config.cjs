a2e1ae6bf4f225d7abef7c0943a57f591e036cc1
Taz <47963508+Web-Dev-Taz@users.noreply.github.com>
2022-06-11 15:44:31 -0600

    added imgs and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+   theme: {
+      extend: {
+         colors: {
+            lightGreen: "#7ed56f",
+            regGreen: "#55c57a",
+            darkGreen: "#28b485",
+         },
+      },
+   },
+   plugins: [],
 }
